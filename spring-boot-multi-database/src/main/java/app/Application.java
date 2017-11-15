b6a2d2e178931405c4e8c53a4e3a4f9dea71c6b7
ZhuKaishengy <13109894018@163.com>
2017-11-15 16:15:09 +0800

    修正

--- spring-boot-multi-database/src/main/java/app/Application.java
+++ spring-boot-multi-database/src/main/java/app/Application.java
@@ -3,9 +3,11 @@ package app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
 @EnableAutoConfiguration
-@ComponentScan(value={"com.aisino.controller","com.aisino.db1.domain","com.aisino.db1.service","com.aisino.db2.domain","com.aisino.db2.service","com.aisino.sysconfig"})
+@ComponentScan(value={"com.aisino.controller","com.aisino.db1","com.aisino.db2","com.aisino.sysconfig"})
 public class Application {
 
 	public static void main(String[] args) throws Exception {
--- spring-boot-multi-database/src/main/java/com/aisino/controller/MultiController.java
+++ spring-boot-multi-database/src/main/java/com/aisino/controller/MultiController.java
@@ -30,12 +30,49 @@ public class MultiController {
 		return Msg.success().add("userList", users).add("testList", tests);
 	}
 	
+	/**
+	 * @Description: 这个一种不正规的写法，事务应该加在service层
+	 * @author zhukaisheng
+	 * @date 2017年11月15日
+	 */
 	@RequestMapping("/add")
 	@Transactional
 	public Msg add(){
 		userService.addUser();
 		System.out.println(1/0);
 		jpTestService.addTest();
+		return Msg.success();
+	}
+	
+	/**
+	 * @Description: 测试分布式访问
+	 * @author zhukaisheng
+	 * @date 2017年11月15日
+	 */
+	@RequestMapping("/assoiation")
+	public Msg addNew(){
+		userService.assoiation();
+		return Msg.success();
+	}
+	
+	/**
+	 * @Description: 测试分布式事务，优先访问自己的数据源
+	 * @author zhukaisheng
+	 * @date 2017年11月15日
+	 */
+	@RequestMapping("/assoiation1")
+	public Msg addNew1(){
+		userService.assoiation1();
+		return Msg.success();
+	}
+	/**
+	 * @Description: 测试分布式事务，优先访问其他的数据源
+	 * @author zhukaisheng
+	 * @date 2017年11月15日
+	 */
+	@RequestMapping("/assoiation2")
+	public Msg addNew2(){
+		userService.assoiation2();
 		return Msg.success();
 	}
 	
